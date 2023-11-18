f32d56e839976233ce59cb8d492c0938ab41589e
Wisnu_Ikhwansyah_Saputra <ikhwansyahwisnu@gmail.com>
2023-11-18 13:40:11 +0700

    Initial commit

---
+++ app/src/main/java/com/hellotoast/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.hellotoast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainHomePage.class));
+                finish();
+            }
+        }, 2000); // Ubah delayMillis menjadi 2000
+    }
+}
